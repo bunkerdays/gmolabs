94e3ea4df9c83e07ed005d7f0f23d4c27a651349
Geoff Morris <geoffm@gmail.com>
2015-07-28 12:05:09 -0700

    added citron and wjperry, image embiggening, white loading bars
    added citron and wjperry, image embiggening on hover, white loading
    bars with grey pulse, nicer.

--- gmolabs-materialize/js/init.js
+++ gmolabs-materialize/js/init.js
@@ -53,16 +53,16 @@
 
     var current_h;
     var current_w;
-    $("img.opener").hover(function(e) {
+    $(".card").hover(function(e) {
         //expand background image
-        current_h = $(this).height();
-        current_w = $(this).width();
-            $(this).stop(true, false).animate({
-                width: (current_w * 1.3),
-                height: (current_h * 1.3)
+        current_h = $(this).find("img.opener").height();
+        current_w = $(this).find("img.opener").width();
+            $(this).find("img.opener").stop(true, false).animate({
+                width: (current_w * 1.1),
+                height: (current_h * 1.1)
             }, 500, 'easeOutExpo');
         }, function() {
-            $(this).stop(true, false).animate({
+            $(this).find("img.opener").stop(true, false).animate({
                 width: current_w + 'px',
                 height: current_h + 'px'
             }, 500, 'easeOutExpo');
